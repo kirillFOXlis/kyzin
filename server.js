7dfefad0b9b041d96abcc661363ca266cd1baeae
kirillFOXlis <keruxa5611@gmail.com>
2025-04-23 22:09:33 +0300

    dvacf

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+// Определение порта, используя переменные окружения Render ($PORT)
+const PORT = process.env.PORT || 3000;
+
+// Запускаем сервер
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
